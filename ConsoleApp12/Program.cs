e99b61eaacae5bc43874aae565c59242ced420f7
Паша Луковников <lukovnikov.pasha@gmail.com>
2021-06-23 18:31:07 +1000

    Изменение

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -8,33 +8,33 @@ namespace ConsoleApp12
     {
         static void Main(string[] args)
         {
-            string path = @"D:\\TestFile2.txt";
-            string path2 = @"D:\\TestFile6.txt";
-            string[] createText = { "Hello", "And", "Welcome", "GoodBye", "or", "new word" };
-            string[] newText = new string[createText.Length];
-            File.WriteAllLines(path, createText, Encoding.UTF8);
-            int count = 0;
-            for (int i = 0; i < createText.Length; i ++)
-            {
-                if (i % 2 == 0)
-                {
-                    int a = i;
-                    //Console.WriteLine(createText[a--]);
-                    newText[count] = createText[a--];
-                    count++;
-                }
-                File.WriteAllLines(path2, newText, Encoding.UTF8);
-            }
+            string path = @"D:\\TestFile10.txt";
+            Console.WriteLine("Введите текст");
+            string[] text = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            File.WriteAllLines(path, text);
+            int a = 0;
             using (StreamReader sr = new StreamReader(path))
             {
                 string line;
-                int a = 0;
+                int count = 0;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    a++;
+                    count++;
                     Console.WriteLine(line);
                 }
-                Console.WriteLine("Количество строк: " + a.ToString());
+                Console.WriteLine("Количество строк в массиве: " + count.ToString());
+            }
+            string[] newtext = new string[text.Length];
+            string path1 = @"D:\\TestFile14.txt";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int b = i;
+                    newtext[a] = text[b--];
+                    a++;
+                }
+                File.WriteAllLines(path1, newtext, Encoding.UTF8);
             }
         }
     }
